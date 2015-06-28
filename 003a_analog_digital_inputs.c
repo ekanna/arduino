c04b29c49714801c3ad0db78e28037bb9fa132de
koti <ekanna.net@gmail.com>
2015-06-29 00:23:09 +0530

    Create 003a_analog_digital_inputs.c

---
+++ 003a_analog_digital_inputs.c
@@ -0,0 +1,14 @@
+// digital inputs/signals have only two states i.e. on or off
+// analog inputs/signals are continuous in nature
+// all natural or real world inputs/signals are analog in nature e.g. sound, temperature, light, air etc..
+
+// voltage is analog in nature
+
+input range ----> 0 to 5 volts
+
+// one byte --> 8 bits --> each bit can indicate two states i.e. on or off
+// possible permtations --> 2^8 --> 256
+
+output range --> 0 to 256
+
+1 volt = 256/5 = 51
