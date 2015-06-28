a45d3c0ee8d395980540cc48ab2587c65509538a
koti <ekanna.net@gmail.com>
2015-06-29 00:25:18 +0530

    Update 003a_analog_digital_inputs.c

--- 003a_analog_digital_inputs.c
+++ 003a_analog_digital_inputs.c
@@ -1,3 +1,5 @@
+// draft notes ....
+
 // digital inputs/signals have only two states i.e. on or off
 // analog inputs/signals are continuous in nature
 // all natural or real world inputs/signals are analog in nature e.g. sound, temperature, light, air etc..
@@ -12,3 +14,5 @@ input range ----> 0 to 5 volts
 output range --> 0 to 256
 
 1 volt = 256/5 = 51
+
+To be continued ...
