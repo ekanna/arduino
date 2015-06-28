912ff831e04ad35e17d3af3319951342a7a5695b
koti <ekanna.net@gmail.com>
2015-06-29 00:33:48 +0530

    Create 007_input_pull_down_pull_up.c

---
+++ 007_input_pull_down_pull_up.c
@@ -0,0 +1,19 @@
+draft notes
+
+// notes on how input pull_down and pull_up works
+
+    ________________
+  |                 |                    -----------------
+  |                 |                   |
+  |Contoller        |----------/ -------| Supply
+  |                 |                   |
+  |                 |                   |-----------------
+  ------------------
+  
+  // noise
+  // GND ground pull down
+  // GND ground pull up
+  // resistors
+  //
+  
+  To be continued
